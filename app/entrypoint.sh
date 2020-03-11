15f1654f3326d5d2a7758a32aa8e5afd5ce8b8b8
Giorgio Consorti <g.consorti@lynxlab.com>
2020-03-11 19:09:27 +0100

    move symlink creation to the proper lines

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -226,11 +226,11 @@ for provider in "${providers[@]}"; do
         if [[ $thirdlevel == "NULL" ]]; then
             $thirdlevel = $provider
         fi
+        thirdlevel=($(echo "${thirdlevel}" | tr -d "'"))
         # symlink $thirdlevel to $provider
         if [[ ! -L ./clients/${thirdlevel} && $thirdlevel != $provider ]]; then
             ln -s -v ./clients/${provider} ./clients/${thirdlevel}
         fi
-        thirdlevel=($(echo "${thirdlevel}" | tr -d "'"))
         if [[ $dotcount -ge 2 ]]; then
             ## PARSED_HOST has 2 or more dots, i.e. is xxxx.domain.ext or yyy.xxxx.domain.ext, etc
             ## substitute everything up to the fisrt '.' with provider in lower case
